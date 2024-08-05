fedf3293f8eae5604e9f34ef108a876f8f77ea80
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-06 03:32:19 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+// const express = require("express");
+// const app = express();
+// const PORT = 4000;
+// const cors = require("cors");
+// const http = require("http");
+// const { Server } = require("socket.io");
+
+// app.use(cors());
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     method: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`Client ${socket.id} connected`);
+
+//   socket.on("privateRoom", (userId) => {
+//     socket.join(userId);
+//     console.log("join room" + userId);
+//   });
+
+//   socket.on("addNote", (userId) => {
+//     socket.broadcast.to(userId).emit("getNote", "Message from server");
+//   });
+//   socket.on("deleteNote", (userId) => {
+//     socket.broadcast.to(userId).emit("getNote");
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log(`Client ${socket.id} disconnected`);
+//   });
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -13,13 +52,22 @@ app.use(
 );
 
 const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*", // Consider restricting this in production
+//     methods: ["GET", "POST"],
+//   },
+// });
+
 const io = new Server(server, {
+  path: "/api/socket",
+  addTrailingSlash: false,
   cors: {
     origin: "*", // Consider restricting this in production
     methods: ["GET", "POST"],
   },
 });
-
+res.socket.server.io = io;
 io.on("connection", (socket) => {
   console.log(`Client ${socket.id} connected`);
 
