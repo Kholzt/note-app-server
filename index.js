7bcc734e0b253d4b403198b407cca4d629170d3b
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-06 03:01:45 +0700

    update origin url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://noteme-123.vercel.app",
+    origin: ["https://noteme-123.vercel.app", "http://localhost:9000"],
     method: ["GET", "POST"],
   },
 });
